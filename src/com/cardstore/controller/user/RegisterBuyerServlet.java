8f876d943832fca029855aafd7883895e988deec
serajeong <135312798+serajeong@users.noreply.github.com>
2024-08-25 01:53:30 +1000

    Update on persistence to match online database

--- src/com/cardstore/controller/user/RegisterBuyerServlet.java
+++ src/com/cardstore/controller/user/RegisterBuyerServlet.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/register")
+@WebServlet("/register_buyer")
 public class RegisterBuyerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/com/cardstore/controller/user/ShowBuyerRegisterFormServlet.java
+++ src/com/cardstore/controller/user/ShowBuyerRegisterFormServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/buyer_register")
+@WebServlet("/register")
 public class ShowBuyerRegisterFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
